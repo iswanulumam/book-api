af8caeb3221937b96c88199afa6bbb9e02dc3ed5
Iswanul Umam <iswanul.umam@gmail.com>
2021-08-21 16:52:32 +0700

    add model customer

---
+++ api/middlewares/jwt_middleware.go
@@ -0,0 +1,27 @@
+package middlewares
+
+import (
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/labstack/echo/v4"
+)
+
+func CreateToken(userId int) (string, error) {
+	claims := jwt.MapClaims{}
+	claims["authorized"] = true
+	claims["userId"] = int(userId)
+	claims["exp"] = time.Now().Add(time.Hour * 1).Unix()
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte("RAHASIA"))
+}
+
+func ExtractTokenUserId(c echo.Context) int {
+	user := c.Get("user").(*jwt.Token)
+	if user.Valid {
+		claims := user.Claims.(jwt.MapClaims)
+		userId := int(claims["userId"].(float64))
+		return userId
+	}
+	return 0
+}
